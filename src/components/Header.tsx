06c6877dba415ed3313c6cb94811aedde34f24c8
Andr213D <68999712+Andr213D@users.noreply.github.com>
2022-07-13 21:32:56 +0300

    Структурировал данные в приложении создал логотип logo.svg заменил lint в package.json

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header: React.FC = () => {
+    return(
+        <header className="App-header">
+            Онлайн КПТ-дневник
+        </header>
+    )
+}
+
+export default Header
