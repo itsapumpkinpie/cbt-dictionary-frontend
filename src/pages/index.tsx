8c6c775a0dd19e362c2806edef0ccda803948b88
Andr213D <68999712+Andr213D@users.noreply.github.com>
2022-08-24 11:59:21 +0300

    Добавил title image, изменил зависимости изображения

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import type { NextPage } from 'next'
+import Image from "next/image";
 
 import styles from '../styles/Home.module.scss'
+import logo from "../assets/img/titleGirl.svg";
 
 const Home: NextPage = () => {
     return (
         <div className={styles.container}>
-            yeep
+            <Image
+                alt="Логотип"
+                src={logo}
+
+            />
         </div>
     )
 }
